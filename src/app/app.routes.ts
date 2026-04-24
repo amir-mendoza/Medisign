5d4222673098d595aae1ff3225a8229146dc7002
Amir Mendoza <U22324716@utp.edu.pe>
2026-04-24 16:11:13 -0500

    Configuracion2

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { HomeBannerdeespera } from './home-bannerdeespera/home-bannerdeespera';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeBannerdeespera }, // ruta principal
+];
